ff8e1c296c1590048c685fcac3817462cc4b62c5
Lesley Sanders <lesleyasanders@gmail.com>
2021-09-27 11:11:08 -0400

    Add input to array method

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,18 @@
 class Translator
-  attr_reader :alphabet
-  
+  attr_reader :alphabet,
+              :night_writer
+
   def initialize
     @alphabet = Alphabet.new
+    @night_writer = NightWriter.new
+  end
+
+  def input_to_array
+    @night_writer.read.chars
+  end
+
+  def convert_to_braille(letter)
+    @alphabet.characters[letter]
   end
 
 end
--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -12,8 +12,19 @@ RSpec.describe 'Translator' do
     translator = Translator.new
 
     expect(translator.alphabet).to be_an_instance_of Alphabet
-    expect(translator.alphabet.characters.key(["0.", "..", ".."])).to eq('a')
+    expect(translator.alphabet.characters.key(['0.', '..', '..'])).to eq('a')
     expect(translator.alphabet.characters.key(['0.', '.0', '00'])).to eq('z')
+  end
 
+  it '#convert_to_braille' do
+    translator = Translator.new
+
+    expect(translator.convert_to_braille('a')).to eq(['0.', '..', '..'])
+  end
+
+  it '#input_to_array' do
+    translator = Translator.new
+
+    expect(translator.input_to_array).to be_an Array
   end
 end
